318eb734bcf0be6f68506ed55217afb80f0110a7
BiKend <bikend.learn@gmail.com>
2024-05-18 13:55:02 +0400

    Добавил конструктор с параметрами по умолчанию в класс Road, что позволяет создавать объекты класса без явного указания значений длины и количества полос при их создании.
    Изменил метод printValues() на const, чтобы указать, что этот метод не изменяет состояние объекта Road и может быть вызван для константных объектов.
    
    В main() заменил присваивание значений полям напрямую на вызов метода setValues(), что делает код более явным и улучшает его читаемость.

--- Ex2.1.cpp
+++ Ex2.1.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
 
-// Класс дороги
+// Класс Дорога
 class Road {
 private:
-  int length; // Длина дороги в метрах
-  int lanes; // Количество полос на дороге
+    int length; // Длина дороги в метрах
+    int lanes; // Количество полос на дороге
 
 public:
-  // Конструктор без параметров
-  Road() {
-    length = 0;
-    lanes = 0;
-  }
+    // Конструктор с параметрами по умолчанию
+    Road(int roadLength = 0, int roadLanes = 0) : length(roadLength), lanes(roadLanes) {}
 
-  // Метод для установки значений полей
-  void setValues(int roadLength, int roadLanes) {
-    length = roadLength;
-    lanes = roadLanes;
-  }
+    // Метод для установки значений полей
+    void setValues(int roadLength, int roadLanes) {
+        length = roadLength;
+        lanes = roadLanes;
+    }
 
-  // Метод для вывода значений полей
-  void printValues() {
-    std::cout << "Длина дороги: " << length << " метров" << std::endl;
-    std::cout << "Количество полос: " << lanes << std::endl;
-  }
+    // Метод для вывода значений полей
+    void printValues() const {
+        std::cout << "Длина дороги: " << length << " метров" << std::endl;
+        std::cout << "Количество полос: " << lanes << std::endl;
+    }
 };
 
 int main() {
-  // Создаем объект дороги с использованием конструктора без параметров
-  Road road1;
-  std::cout << "Значения полей после создания объекта:" << std::endl;
-  road1.printValues();
+    // Создаем объект дороги с использованием конструктора с параметрами по умолчанию
+    Road road1;
+    std::cout << "Значения полей после создания объекта:" << std::endl;
+    road1.printValues();
 
-  // Присваиваем значения полям
-  road1.setValues(1000, 2);
+    // Присваиваем значения полям с помощью метода setValues()
+    road1.setValues(1000, 2);
 
-  // Выводим значения полей после присваивания
-  std::cout << "Значения полей после присваивания:" << std::endl;
-  road1.printValues();
+    // Выводим значения полей с помощью метода printValues()
+    std::cout << "Значения полей после присваивания:" << std::endl;
+    road1.printValues();
 
-  return 0;
+    return 0;
 }
